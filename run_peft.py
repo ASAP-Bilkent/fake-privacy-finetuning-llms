e622e412938d3d0c71203db69a44c1b187ccbf95
mpcl5 <m.poorghaffar@bilkent.edu.tr>
2024-12-28 11:54:12 +0100

    added run_peft

---
+++ run_peft.py
@@ -0,0 +1,199 @@
+from datasets import load_dataset, Dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from datasets import load_dataset, Dataset
+from accelerate import PartialState
+import os
+import argparse
+import re
+import sys
+from trl import SFTTrainer
+from peft import LoraConfig, get_peft_model
+import torch
+
+RESULTS_DIR = "./"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", help="model that will be instruction-tuned",
+                        default="EleutherAI/pythia-14m")
+    parser.add_argument("-i", "--input", help="training dataset path",
+                        default="./enron.jsonl")
+    parser.add_argument("-pdbs", "--per_device_batch_size", help="",
+                        type=int, default=1)
+    parser.add_argument("-gas", "--gradient_accumulation_steps", help="",
+                        type=int, default=8)
+    parser.add_argument("-w", "--warmup_steps", help="",
+                        type=int, default=0)
+    parser.add_argument("-e", "--epochs", help="",
+                        type=int, default=4)
+    parser.add_argument("-lr", "--learning_rate", help="",
+                        type=float, default=2e-6)
+    parser.add_argument("-l", "--logging_steps", help="",
+                        type=int, default=1)
+    parser.add_argument("-opt", "--optimizer", help="",
+                        default="adamw_torch")
+    parser.add_argument("-wd", "--weight_decay", help="",
+                        type=float, default=0)
+    parser.add_argument("-s", "--scheduler", help="",
+                        default="linear")
+    parser.add_argument("-dt", "--distribution_type", help="It can be ddp, mp or fsdp",
+                        type=str, default="ddp")
+
+    return parser.parse_args()
+
+
+def get_output_dir(model_name, wd, lr, is_original):
+    """
+    Generates an output directory path based on model name, weight decay, learning rate, and originality status.
+
+    Args:
+        model_name (str): Name of the model.
+        wd (float): Weight decay.
+        lr (float): Learning rate.
+        is_original (bool): Flag indicating whether the model is original.
+
+    Returns:
+        str: The path to the output directory.
+    """
+    # Extract the base name of the model
+    output_directory = re.search(r'[^/]+$', model_name, re.MULTILINE).group(0)
+
+    # Append additional directory structure based on the is_original flag
+    if is_original:
+        output_directory = os.path.join(output_directory, "self-generated-dora-" +
+                                        re.search(r'/(pythia[^/]*|opt-[^/]*)', args.input).group(1))
+    else:
+        output_directory = os.path.join(output_directory, "original-dora")
+
+    # Add weight decay and learning rate information
+    output_directory = os.path.join(output_directory, f"{wd}_{lr}")
+
+    # Create the results directory using os.path
+    results_dir = os.path.join(RESULTS_DIR, output_directory)
+    os.makedirs(results_dir, exist_ok=True)
+
+    return results_dir
+
+
+def formatting_prompts_func(example, eos_token):
+    exampleGenerationPrompt = "{prompt}{continuation}"
+
+    example["text"] = exampleGenerationPrompt.format(
+        prompt=example["prompt"],
+        continuation=example["continuation"]
+    ) + eos_token
+
+    return example
+
+
+def get_dataset(input):
+    try:
+        dataset = load_dataset("json", data_files=input, split="train")
+    except:
+        dataset = Dataset.from_file(input)
+
+    dataset = dataset.select(range(len(dataset) - 2220, len(dataset)))
+
+    return dataset
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {
+            all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+def format_rate(number):
+    return format(float(number), ".0e")
+
+
+def get_device_map(distri_type):
+    if distri_type == "ddp":
+        device_index = PartialState().process_index
+        return {"": device_index}
+
+    if distri_type == "mp":
+        return "auto"
+
+    return None
+
+
+def get_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    
+    return tokenizer
+
+
+def tokenize_function(item, tokenizer):
+        item = tokenizer(
+            item["text"],
+            truncation=True,
+            padding="max_length",
+            max_length=2048,
+        )
+        item["labels"] = item["input_ids"]
+        return item
+
+def prepare_dataset(dataset, tokenizer):
+    eos_token = tokenizer.eos_token
+    # If no text column is provided.
+    if 'text' not in dataset.column_names:
+        dataset = dataset.map(lambda item: formatting_prompts_func(item, eos_token))
+        
+    dataset = dataset.map(
+        lambda item: tokenize_function(item, tokenizer),
+        remove_columns=dataset.column_names,
+        batched=True,
+    )
+    
+    return dataset
+
+    
+
+def run_peft(args):
+    wd = format_rate(args.weight_decay)
+    lr = format_rate(args.learning_rate)
+
+    output_directory = get_output_dir(
+        args.model, wd, lr, 'continuations' in args.input)
+
+    # TODO: remove this. Add wandb's logging.
+    sys.stdout = open(output_directory + "trajectory.txt",
+                      "w")  # override print
+
+    dataset = get_dataset(args.input)
+    device_map = get_device_map(args.distribution_type)
+    
+    
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model,
+        trust_remote_code=False,
+        cache_dir="",
+        device_map=device_map,
+        # device_map="auto",
+        use_cache=False,
+    )
+    
+    if args.distribution_type != "fsdp" :
+        model.gradient_checkpointing_disable()
+        
+    tokenizer = get_tokenizer(args.model)
+    dataset = prepare_dataset(dataset, tokenizer)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    run_peft(args)
