f52397b69b000ca3306c2d24e651e50a65a30767
mpcl5 <m.poorghaffar@bilkent.edu.tr>
2025-01-17 14:24:02 +0100

    refactored perplexity

--- perplexity/perplexity.py
+++ perplexity/perplexity.py
@@ -1,15 +1,23 @@
 from evaluate import load
-from datasets import load_dataset, Dataset
-import torch
+from datasets import load_dataset
 import argparse
 import os
 import sys
 import logging
 
-# TODO: refactor this.
 
 _original_stdout = sys.stdout
 _original_stderr = sys.stderr
+
+
+def parse_argument():
+    parser = argparse.ArgumentParser()
+
+    # TODO: Add an argument to control the output
+    parser.add_argument("dir", help="Directory that includes the models to be ran.",
+                        default="../pythia-1.4b")
+
+    return parser.parse_args()
 
 
 def block_output():
@@ -27,48 +35,33 @@ def enable_output(dir):
 
 def process_checkpoint(checkpoint_path, output_dir):
     block_output()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    
     dataset = load_dataset("json", data_files='../enron.jsonl', split="train")
-
     dataset = dataset.select(range(0, 1110))
-
-    def createText(x):
-        x['text'] = x['prompt'] + x['continuation']
-
-        return x
-
-    dataset = dataset.map(createText)
+    dataset = dataset.map(lambda x: x['prompt'] + x['continuation'])
 
     perplexity = load("perplexity", module_type="metric")
-    results = perplexity.compute(
-        predictions=dataset['text'], model_id=checkpoint_path)
+    results = perplexity.compute(predictions=dataset['text'], model_id=checkpoint_path)
 
     enable_output(output_dir)
 
-    print("model path: " + str(checkpoint_path) +
-          "; mean perplexity:" + str(results['mean_perplexity']))
+    # TODO: replace this with a rational system.
+    print("model path: " + str(checkpoint_path) +"; mean perplexity:" + str(results['mean_perplexity']))
 
     block_output()
 
 
 def traverse_checkpoints(base_path, output_dir):
-    for root, dirs, files in os.walk(base_path):
-        if "/self-generated-dora-pythia-12b/" in root:
-            for dir_name in dirs:
-                full_path = os.path.join(root, dir_name)
-                if dir_name.startswith("checkpoint-"):
-                    print(f"root is {full_path}")
-                    process_checkpoint(full_path, output_dir)
+    for root, dirs, _ in os.walk(base_path):
+        for dir_name in dirs:
+            if dir_name.startswith("checkpoint-"):
+                checkpoint_path = os.path.join(root, dir_name)
+                print(f"root is {checkpoint_path}")
+                process_checkpoint(checkpoint_path, output_dir)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("dir", help="Directory that includes the models to be ran.",
-                        default="../pythia-1.4b")
-
-    args = parser.parse_args()
+    args = parse_argument()
 
     os.makedirs(args.dir, exist_ok=True)
 
